f9ee1a1e16ba80e1d0b5774db2953e02f97649c8
Josh Harmon <josh.harmon@xero.com>
2019-07-11 13:03:37 -0700

    Added O(n^3) solution for 3Sum

--- LeetCode.Solutions/ThreeSum_15.cs
+++ LeetCode.Solutions/ThreeSum_15.cs
@@ -29,48 +29,85 @@ namespace LeetCode.Solutions
         public static IList<IList<int>> ThreeSum(int[] nums)
         {
             if (nums == null || nums.Length == 0)
-                return null;
+                return new List<IList<int>>();
 
             if (nums.Length < 2)
-                return null;
+                return new List<IList<int>>();
 
             if (nums.Length == 3)
             {
-                if (nums[0] + nums[1] + nums[2] == 0)
-                {
-                    return new List<IList<int>> { nums.ToList() };
-                }
-
-                return null;
+                return nums[0] + nums[1] + nums[2] == 0
+                    ? new List<IList<int>> { nums.ToList() }
+                    : new List<IList<int>>();
             }
-            var triplets = new List<IList<int>>();
 
-            var a = (Pos: 0, Val: nums[0]);
-            var b = (Pos: 1, Val: nums[1]);
-            var c = (Pos: 2, Val: nums[2]);
+            var triplets = new HashSet<Triplet>();
 
             bool ZeroSum(int x, int y, int z) => x + y + z == 0;
 
-            for(var i = 0; i < nums.Length; ++i)
+            for (var i = 0; i < nums.Length; ++i)
             {
-                if (ZeroSum(a.Val,b.Val,c.Val))
+                var a = nums[i];
+                for (var j = i + 1; j < nums.Length; ++j)
                 {
-                    triplets.Add(new[] { a.Val, b.Val, c.Val });
-                }
-                while (c.Pos < nums.Length)
-                {
-                    if (ZeroSum(a.Val, b.Val, c.Val))
+                    var b = nums[j];
+                    for (var k = j + 1; k < nums.Length; ++k)
                     {
-                        triplets.Add(new[] { a.Val, b.Val, c.Val });
+                        var c = nums[k];
+
+                        if (!ZeroSum(a, b, c))
+                        {
+                            continue;
+                        }
+
+                        var triplet = new Triplet(a, b, c);
+                        if (!triplets.Contains(triplet))
+                        {
+                            triplets.Add(triplet);
+                        }
                     }
-
-                    c.Pos++;
-                    c.Val = nums[c.Pos];
                 }
-
             }
 
-            return triplets;
+            return triplets.Select(t => new List<int> { t.A, t.B, t.C }).Cast<IList<int>>().ToList();
+        }
+
+        public struct Triplet
+        {
+            public Triplet(int a, int b, int c)
+            {
+                A = a;
+                B = b;
+                C = c;
+
+                Counts = new Dictionary<int, int> { { a, 1 } };
+                if (Counts.ContainsKey(b))
+                    Counts[b] += 1;
+                else
+                    Counts.Add(b, 1);
+                if (Counts.ContainsKey(c))
+                    Counts[c] += 1;
+                else
+                    Counts.Add(c, 1);
+            }
+
+            public int A { get; }
+            public int B { get; }
+            public int C { get; }
+
+            public readonly IDictionary<int, int> Counts;
+
+            // HashCode shouldn't be used for equality check...
+            public override bool Equals(object o) => GetHashCode() == (o as Triplet? ?? default).GetHashCode();
+
+            public override int GetHashCode()
+            {
+                var countStr = Counts
+                               .OrderBy(kv => kv.Key)
+                               .Select(kv => $"{kv.Key}{kv.Value}")
+                               .Aggregate((a, s) => a += s);
+                return countStr.GetHashCode();
+            }
         }
     }
 }
--- LeetCode.Tests/ThreeSum_15_Tests.cs
+++ LeetCode.Tests/ThreeSum_15_Tests.cs
@@ -1,7 +1,10 @@
 ﻿using System;
-using System.Linq;
 using System.Collections.Generic;
-
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Timers;
 using Xunit;
 using static LeetCode.Solutions.ThreeSum_15;
 
@@ -11,10 +14,15 @@ namespace LeetCode.Tests
     {
         [Theory]
         [InlineData("-1,0,1,2,-1,-4", "-1,0,1;-1,-1,2")]
+        [InlineData("3,0,-2,-1,1,2", "3,-2,-1;0,-2,2;0,-1,1")]
         public void ThreeSum_Tests(string nums, string expected)
         {
             var n = nums.Split(",").Select(int.Parse).ToArray();
-            var exp = expected.Split(";").Select(s => s.Split(",").Select(int.Parse).ToList()).ToList();
+            var exp = expected.Split(";")
+                    .Select(s => s.Split(",")
+                    .Select(int.Parse)
+                    .ToList())
+                .ToList();
 
             var actual = ThreeSum(n);
 
@@ -22,13 +30,40 @@ namespace LeetCode.Tests
             Assert.NotEmpty(actual);
             Assert.Equal(exp.Count, actual.Count);
 
-            actual.OrderBy(l => l[0]);
-            exp.OrderBy(l => l[0]);
+            var actualOrdered = actual.Select(t => t.OrderBy(li => li).ToList()).OrderBy(l => l[0]).ToList();
+            var expOrdered = exp.Select(t => t.OrderBy(li => li).ToList()).OrderBy(l => l[0]).ToList();
 
             for (var i = 0; i < exp.Count; ++i)
             {
-                Assert.True(exp[i].SequenceEqual(actual[i]));
+                Assert.True(expOrdered[i].SequenceEqual(actualOrdered[i]));
             }
+        }
+        [Theory]
+        [InlineData("-1,0,1,2,-1,-4", 50)]
+        [InlineData("13,5,-4,-9,1,-3,10,-7,7,3,1,-13,-11,7,-10,12,-15,13,5,-8,-11," +
+                    "-12,-15,-13,-3,-13,5,-4,6,1,-10,4,13,-7,3,-9,-3,-2,-1,12,9,-15," +
+                    "14,5,0,-10,-5,-8,-12,-15,-1,-8,11,-9,-14,-7,-6,7,-4,-15,-15,-7," +
+                    "-4,14,1,6,12,14,12,-11,11,-2,11,2,-12,-4,7,-2,-5,10,-9,10,9,-13," +
+                    "-14,11,-13,-13,3,-1,9,3,7,-9,-6,-14,4,-8,7,1,-12,-5,14,14,12,10," +
+                    "-12,-3,-7,-2,-8,-9,-7,9,-7,-13,5,-12,-11,-7,2,14,3,-14", 100)]
+        public async Task ThreeSum_Perf_Tests(string nums, int maxMilliseconds)
+        {
+            var n = nums.Split(",").Select(int.Parse).ToArray();
+
+            IList<IList<int>> actual = null;
+            var sw = new Stopwatch();
+            var actualTask = Task.Run(() =>
+            {
+                sw.Start();
+                actual = ThreeSum(n);
+                sw.Stop();
+            });
+            var delayTask = Task.Delay(maxMilliseconds, new CancellationToken());
+            var completedTask = await Task.WhenAny(actualTask, delayTask);
+
+            Assert.True(completedTask.Id== actualTask.Id,
+                        $"Elapsed time (ms): '{sw.ElapsedMilliseconds}'");
+            Assert.NotNull(actual);
         }
     }
 }
