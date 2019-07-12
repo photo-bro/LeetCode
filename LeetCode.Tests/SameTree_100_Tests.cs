b44e745f49ee05d803b760dca2bdb60b74818e8c
Josh Harmon <josh.harmon@xero.com>
2019-07-12 10:46:35 -0700

    Started #100 SameTree

---
+++ LeetCode.Tests/SameTree_100_Tests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using LeetCode.Solutions;
+using Xunit;
+using static LeetCode.Solutions.SameTree_100;
+
+namespace LeetCode.Tests
+{
+    public class SameTree_100_Tests
+    {
+        [Theory]
+        [InlineData("","","")]
+        public void IsSameTree_Test(string p, string q, string exp)
+        {
+            var expected = new TreeNode(0);
+
+            var actual = IsSameTree(null, null);
+
+        }
+    }
+}
