e37231f9f8d3114ae656a34ca1f03375c091d0d0
Josh Harmon <photigragraphy@gmail.com>
2019-07-15 09:58:17 -0700

    Solved #100

--- LeetCode.Solutions/Easy/SameTree_100.cs
+++ LeetCode.Solutions/Easy/SameTree_100.cs
@@ -1,4 +1,7 @@
-﻿namespace LeetCode.Solutions.Easy
+﻿using System.Linq;
+using System;
+
+namespace LeetCode.Solutions.Easy
 {
     ////// S a m e   T r e e - E A S Y  //////
     ///
@@ -32,20 +35,77 @@
     ///    Output: false
     ///    
     ////// S a m e   T r e e - E A S Y  //////
-
     public static class SameTree_100
     {
         public static bool IsSameTree(TreeNode p, TreeNode q)
         {
-            return false;
+            if (p == null && q == null)
+            {
+                return true;
+            }
+
+            if (p == null || q == null)
+            {
+                return false;
+            }
+
+            if (p.val != q.val)
+            {
+                return false;
+            }
+
+            var left = IsSameTree(p.left, q.left);
+            var right = IsSameTree(p.right, q.right);
+            return left && right;
         }
 
         public class TreeNode
         {
-            public int Val;
-            public TreeNode Left;
-            public TreeNode Right;
-            public TreeNode(int x) { Val = x; }
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+            public TreeNode(int x) { val = x; }
+
+            public static TreeNode StringToTree(string s)
+            {
+                if (string.IsNullOrEmpty(s))
+                    return null;
+
+                var ints = s.Split(",").Select(i => i == "null" ? (int?)null : int.Parse(i)).ToArray().AsSpan();
+
+                if (ints.Length < 1)
+                    return null;
+
+                var tree = StringToTreeImpl(ints.Slice(1), new TreeNode(ints[0].Value));
+
+                return tree;
+            }
+
+            internal static TreeNode StringToTreeImpl(Span<int?> values, TreeNode head = null)
+            {
+                if (values.Length == 0)
+                    return head;
+
+                if (values.Length > 0 && values[0].HasValue)
+                    head.left = new TreeNode(values[0].Value);
+
+                if (values.Length > 1 && values[1].HasValue)
+                    head.right = new TreeNode(values[1].Value);
+
+                //if (values.Length > 2 && values[3].HasValue)
+                //{
+                //    head.left.left = new TreeNode(values[3].Value);
+                //    head.left.left = StringToTreeImpl(values.Slice(3), head.left.left);
+                //}
+
+                //if (values.Length > 3 && values[4].HasValue)
+                //{
+                //    head.left.left = new TreeNode(values[4].Value);
+                //    head.left.left = StringToTreeImpl(values.Slice(4), head.left.left);
+                //}
+
+                return head;
+            }
         }
     }
 }
--- LeetCode.Tests/Easy/SameTree_100_Tests.cs
+++ LeetCode.Tests/Easy/SameTree_100_Tests.cs
@@ -1,18 +1,24 @@
-﻿using LeetCode.Solutions.Easy;
-using Xunit;
+﻿using Xunit;
+
+using static LeetCode.Solutions.Easy.SameTree_100;
 
 namespace LeetCode.Tests.Easy
 {
     public class SameTree_100_Tests
     {
         [Theory]
-        [InlineData("","","")]
-        public void IsSameTree_Test(string p, string q, string exp)
+        [InlineData("1,2,3", "1,2,3", true)]
+        [InlineData("1,2,1", "1,1,2", false)]
+        [InlineData("1,2", "1,null,2", false)]
+        public void IsSameTree_Test(string p, string q, bool expected)
         {
-            var expected = new SameTree_100.TreeNode(0);
+            var l = TreeNode.StringToTree(p);
+            var r = TreeNode.StringToTree(q);
 
-            var actual = SameTree_100.IsSameTree(null, null);
+            var actual = IsSameTree(l, r);
 
+            Assert.Equal(expected, actual);
         }
+
     }
 }
