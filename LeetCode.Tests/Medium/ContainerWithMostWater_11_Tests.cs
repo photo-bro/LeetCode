2e22005e354e598a506d84bab64bf26d693b8cb9
Josh Harmon <photigragraphy@gmail.com>
2019-07-22 08:54:19 -0700

    Added frame for #11 Container with most water

---
+++ LeetCode.Tests/Medium/ContainerWithMostWater_11_Tests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using Xunit;
+using static LeetCode.Solutions.Medium.ContainerWithMostWater_11;
+
+namespace LeetCode.Tests.Medium
+{
+    public class ContainerWithMostWater_11_Tests
+    {
+        [Theory]
+        [InlineData("1,8,6,2,5,4,8,3,7", 49)]
+        public void MaxArea_Tests(string heights, int expected)
+        {
+            var h = heights.Split(',').Select(int.Parse).ToArray();
+            var actual = MaxArea(h);
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
